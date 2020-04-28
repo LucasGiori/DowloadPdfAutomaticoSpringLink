b0af953ef7ac4470739c9e0da1b9b91b5a0f40f8
Lucas Giori Cesconetto <34003961+LucasGiori@users.noreply.github.com>
2020-04-28 18:21:27 -0300

    Comentando Código

--- scraping_spring_link.py
+++ scraping_spring_link.py
@@ -1,29 +1,31 @@
-import requests
+import requests,sys,os
 from bs4 import BeautifulSoup
 from urls import getUrl
 
-urls=getUrl()
-for i in urls:
+#Pasta onde irá salvar o arquivo, pega a pasta raiz do Script Python,em seguida defino em qual pasta será
+downloadPath = str(sys.path[0])+'/arquivos/' 
+
+urls=getUrl() #Função que faz as leituras dos link no arquivo de texto e retorna uma lista
+for url in urls:#for para percorrer a lista
     try:
-        print("\nLink Page Pdf: ",i,"\n")
-        url = i#'http://link.springer.com/openurl?genre=book&isbn=978-0-306-48048-5'
+        print("\nLink Page Pdf: ",url,"\n")
         soup = BeautifulSoup(requests.get(url).text,'lxml')
 
-
+        #Aqui estamos Fazendo o parser do Html, e buscando pela class page-title, onde é definido o nome do livro ou pdf
         tag_title = str(soup.find("div", {"class" : "page-title"}).getText())
-        tag_title = tag_title.replace('\n','')
-        #page = soup.find('p')
-        #print(tag_title)
+        tag_title = tag_title.replace('\n','')#substituindo o \n (Quebra de linha) para não dar erro quando for salvar o arquivo
+        
 
-        tags=soup.find("div", {"class" : "cta-button-container__item"})
-        for i in tags.find_all('a',href=True):
-            link='http://link.springer.com'+i["href"]
-            name_arquivo = i["href"].split('/')[-1]
-            response = requests.get(link)
+        tags=soup.find("div", {"class" : "cta-button-container__item"})#Fazendo o parser e buscando pela class onde contém o link de dowload do pdf
+        for i in tags.find_all('a',href=True):#percorrendo o html que foi encontrado, e buscando pela tag "a"
+            link='http://link.springer.com'+i["href"]#i["href"] é o atributo que está o link do pdf
+            
+            response = requests.get(link)#aqui realiza um requisição para busar o pdf
             print("Nome Arquivo: ",tag_title)
-            print("Link Download PDF: ",name_arquivo)
+            print("Link Download PDF: ",link)
 
-            with open(tag_title+'.pdf', 'wb') as f:
-                f.write(response.content)
-    except:
-        print("\nOcorreu um erro, ao tentar fazer o download: ",i,"\n\n")
+            with open(downloadPath+tag_title+'.pdf', 'wb') as f:#aqui será onde iremos criar o arquivo com o path especificado
+                f.write(response.content)#aqui salvamos o conteudo da requisição no arquivo criado
+    except Exception as e: #caso aconteça algum erro ele entra no except, para não para a execução.
+        print("\nOcorreu um erro, ao tentar fazer o download: ",url," Erro",e,"\n\n")
+
